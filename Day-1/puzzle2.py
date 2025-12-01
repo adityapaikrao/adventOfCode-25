5e1e7fdba79e885e15415ed819e4c98c90ee1ad4
adityapaikrao <paikrao.aditya@gmail.com>
2025-12-01 14:01:40 -0500

    fixing ignore for .py files

---
+++ Day-1/puzzle2.py
@@ -0,0 +1,35 @@
+from typing import List, Tuple
+
+
+def get_password(rotations: List[Tuple[int, int]]) -> int:
+    prev_pos = 50
+    num_zeroes = 0
+
+    for dir, rotation in rotations:
+        # add in complete circles of rotation
+        num_zeroes += rotation // 100
+        rotation %= 100
+
+        # add in zeroes during rotation
+        new_pos = prev_pos + (dir * rotation)
+        if (new_pos < 0 or new_pos > 100) and prev_pos != 0: 
+            num_zeroes += 1
+
+        new_pos = (new_pos + 100) % 100
+        
+        if new_pos == 0 and prev_pos != 0:
+            num_zeroes += 1
+        
+        prev_pos = new_pos
+
+    return num_zeroes
+
+if __name__ == "__main__":
+
+    rotations = []
+    with open("./puzzle1.in", "r") as f:
+        for line in f.readlines():
+            dir = 1 if line[0] == "R" else -1
+            rotations.append((dir, int(line[1:])))
+    
+    print(get_password(rotations))
