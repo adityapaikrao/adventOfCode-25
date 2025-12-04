f7f8f47383c09607a35dd1b33294984c42e0bd61
adityapaikrao <paikrao.aditya@gmail.com>
2025-12-04 15:20:55 -0500

    refactor: streamline functions and improve parameter handling in puzzle2

--- Day-4/puzzle2.py
+++ Day-4/puzzle2.py
@@ -3,61 +3,49 @@ from typing import List, Tuple
 from itertools import product
 from collections import deque
 
-def is_valid(x: int, y: int) -> bool:
+def is_valid(x: int, y: int, N: int, M: int) -> bool:
     return 0 <= x < N and 0 <= y < M
 
-def get_num_nbrs(x: int, y: int) -> int:
-    
+def get_num_nbrs(grid: List[List[str]], x: int, y: int, directions: List[Tuple[int, int]], N: int, M: int) -> int:
     num_nbrs = 0
     for dx, dy in directions:
         new_x = x + dx
         new_y = y + dy
-
-        if is_valid(new_x, new_y) and grid[new_x][new_y] == "@":
+        if is_valid(new_x, new_y, N, M) and grid[new_x][new_y] == "@":
             num_nbrs += 1
-    
     return num_nbrs
 
-
-def get_removable_rolls(max_nbrs: int = 3) -> List[Tuple[int, int]]:
+def get_removable_rolls(grid: List[List[str]], directions: List[Tuple[int, int]], N: int, M: int, max_nbrs: int = 3) -> List[Tuple[int, int]]:
     removable_rolls = []
-
     for i in range(N):
         for j in range(M):
-            if grid[i][j] != "@": continue
-        
-            num_nbrs = get_num_nbrs(i, j)
-            if num_nbrs <= max_nbrs: 
+            if grid[i][j] != "@":
+                continue
+            if get_num_nbrs(grid, i, j, directions, N, M) <= max_nbrs:
                 removable_rolls.append((i, j))
-    
     return removable_rolls
 
+def remove_rolls(grid: List[List[str]], max_nbrs: int = 3) -> int:
+    N, M = len(grid), len(grid[0])
+    directions = [(di, dj) for di, dj in product([-1, 0, 1], repeat=2) if (di, dj) != (0, 0)]
 
-def remove_rolls(max_nbrs: int = 3):
     q = deque()
     num_removed = 0
 
-    q.extend(get_removable_rolls(max_nbrs))
+    q.extend(get_removable_rolls(grid, directions, N, M, max_nbrs))
     num_removed += len(q)
     print(f"Added {len(q)} roll(s) to be removed")
 
-    while q: 
+    while q:
         for _ in range(len(q)):
             curr_x, curr_y = q.popleft()
             grid[curr_x][curr_y] = "."
-        
-        q.extend(get_removable_rolls(max_nbrs))
+        q.extend(get_removable_rolls(grid, directions, N, M, max_nbrs))
         num_removed += len(q)
         print(f"Added {len(q)} roll(s) to be removed")
-    
+
     return num_removed
 
-
 if __name__ == "__main__":
-    global N, M, directions, grid
-
     grid = parse_input()
-    directions = [(di, dj) for di, dj in product([-1, 0, 1], repeat=2) if (di, dj) != (0, 0)]
-    N, M  = len(grid), len(grid[0])
-
-    print(remove_rolls(max_nbrs=3))
+    print(remove_rolls(grid, max_nbrs=3))
